d0ef5574f02777911968b204274a62b6f9015113
kalebishop <kaleb.bishop@colorado.edu>
2024-07-08 10:03:13 -0600

    added testing of nursing-mds

--- scripts/textual_difference.py
+++ scripts/textual_difference.py
@@ -3,7 +3,7 @@ import json
 OPENAI_API_KEY = "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"
 
 client = OpenAI(api_key=OPENAI_API_KEY)
-MODEL  = "gpt-4-turbo"
+MODEL  = "gpt-3.5-turbo"
 
 PATIENT_FILE_NAME = "data/ipass_file_1.txt" # txt file, any format
 HANDOVER_FILE_NAME = "data/ipass_handover_1_modified.json" # json file, contains "report" and "response" fields
@@ -16,21 +16,12 @@ def read_handover_file(file_name):
     with open(file_name, "r") as f:
         return json.load(f)
     
+def load_mds():
+    with open("data/mds.json", "r") as f:
+        return json.load(f)
+    
 def gpt_response(system_role_message, prompt):
-    response = client.chat.completions.create(
-        model=MODEL,
-        messages=[
-            {"role": "system", "content": system_role_message},
-            {"role": "user", "content": prompt}
-        ]
-        # temperature=0.5
-    )
-    return response
-
-def one_shot_comparison(patient_record, handover_transcript):
-    system_role_message = """
-    You are a medical professional assisting in a nursing handover. Concisesly list any important patient information that is missing from the handover transcript.
-    """
+    response = offload_buffers=True, 
 
     prompt = f"""
     **Patient record:**
@@ -69,6 +60,21 @@ def distill_from_emr(patient_record):
     response = gpt_response(system_role_message, prompt)
     return response
 
+def process_emr_to_mds(patient_record, mds):
+    system_role_message = """
+        Fill out the below handover form with any applicable key patient information from the following patient record.
+    """
+
+    prompt = f"""
+    **Handover data:**
+    {mds}
+    **Patient record:**
+    {patient_record}
+    """
+
+    response = gpt_response(system_role_message, prompt)
+    return response
+
 def compare_summaries(record_summary, transcript_summary):
     system_role_message = """
         Compare the two sets of key patient information and list any discrepancies that should be known by an incoming nurse.
@@ -92,8 +98,11 @@ if __name__ == "__main__":
 
     # response = one_shot_comparison(patient_record, handover_transcript)
     
-    transcript_summary = distill_from_transcript(handover_transcript).choices[0].message.content
-    record_summary = distill_from_emr(patient_record).choices[0].message.content
-    response = compare_summaries(record_summary, transcript_summary)    
+    # transcript_summary = distill_from_transcript(handover_transcript).choices[0].message.content
+    # record_summary = distill_from_emr(patient_record).choices[0].message.content
+    # response = compare_summaries(record_summary, transcript_summary)    
 
-    print(response.choices[0].message.content)
+    mds = load_mds()
+    response = process_emr_to_mds(patient_record, mds)
+
+    print(response.choices[0].message.content)  
