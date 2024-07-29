f23b6ba8767fd737179917f07582389924bd6fc4
kalebishop <kaleb.bishop@colorado.edu>
2024-07-29 17:05:43 -0600

    fixed tile scaling

--- evaluation/game/visualization/state_visualizer.py
+++ evaluation/game/visualization/state_visualizer.py
@@ -2,6 +2,7 @@ import pygame
 from evaluation.game.visualization.utils import *
 from evaluation.game.scripts.game_mdp import Direction
 import os
+import copy
 
 GRAPHICS_DIR = "./evaluation/game/assets/"
 
@@ -11,12 +12,32 @@ WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 
 class StateVisualizer:
+    DEFAULT_VALUES = {
+        "height": None,
+        "width": None,
+        "tile_size": 96,
+        "window_fps" : 30,     
+    }
+
     def __init__(self, **kwargs):
         self.PLAYER_IMG = MultiFramePygameImage(os.path.join(GRAPHICS_DIR, "000.png"), os.path.join(GRAPHICS_DIR, "char_sprites.json"))
         self.UNSCALED_TILE_SIZE = 32
         self.GRASS_TILE = pygame.image.load(os.path.join(GRAPHICS_DIR, "grass.png")).convert_alpha()
         self.GRASS_TILE = pygame.transform.scale(self.GRASS_TILE, (self.UNSCALED_TILE_SIZE, self.UNSCALED_TILE_SIZE))
-        self.scale_by_factor = 1
+
+        params = copy.deepcopy(self.DEFAULT_VALUES)
+        params.update(kwargs)
+        self.configure(**params)
+
+    @classmethod
+    def configure_defaults(cls, **kwargs):
+        cls._check_config_validity(kwargs)
+        cls.DEFAULT_VALUES.update(copy.deepcopy(kwargs))
+
+    def configure(self, **kwargs):
+        # StateVisualizer._check_config_validity(kwargs)
+        for param_name, param_value in copy.deepcopy(kwargs).items():
+            setattr(self, param_name, param_value)
 
     def display_rendered_state(self, state, grid=None):
         surface = self.render_state(state, grid)
@@ -57,6 +78,10 @@ class StateVisualizer:
         """
         (x,y) = position
         return (self.tile_size * x, self.tile_size * y)
+    
+    @property
+    def scale_by_factor(self):
+        return self.tile_size/self.UNSCALED_TILE_SIZE
 
     def render_state(self, state, grid=None):
         pygame.init()
