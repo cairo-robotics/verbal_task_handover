bfb014948b93c7407abbe6703600dc9a9ce5a3c0
kalebishop <kabi2483@colorado.edu>
2024-09-18 16:16:24 -0600

    openai integration & human chat loop

--- src/graph.py
+++ src/graph.py
@@ -240,9 +240,94 @@ def test_graph_updates():
         graph = TelemetryGraph()
         graph.parse_from_string(s)
 
+
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationChain
+
+from colorama import Fore, Style
+from queue import Queue
+from abc import ABC
+
+class textBot(ABC):
+    def __init__(self) -> None:
+        super().__init__()
+        self.API_KEY            =  os.environ['OPENAI_API_KEY']
+        self.memory             =  ConversationBufferMemory()
+        self.llm                =  ChatOpenAI(model='gpt-3.5-turbo')
+        self.conversation       =  ConversationChain(
+                                        llm=self.llm,
+                                        memory=self.memory
+                                        )
+        self.nodes_logs         =  None
+
+    def paste_context(self, graph):
+        self.message += "\n" + str(graph)
+
+    def chatbot_prompt(self):
+        # self.message += "\nGiven this information, you're now given a human user who has observed the entire experiment happen, by taking inputs from the user you have to perform a root cause analysis on the behaviortree and suggest changes in a final report. You can ask the user questions in one by one manner, you can start by asking how did the experiment go, followed by subsequent questions that you think will be helpful for debugging the system."
+        self.message += "\nGiven the above networkX graph of the current game states, please provide an answer to the user's questions."
+
+    def bot_loop(self, graph):
+        """_summary_
+        """
+
+        self.message = ""
+        self.paste_context(graph)
+        self.chatbot_prompt()
+        save_conversation = ""
+        save_conversation += "User response: " + self.message + "\n \n"
+        # Reply after conditioning GPT to be a chat agent
+        reply = self.conversation.predict(input=self.message)
+        save_conversation += "AI response: " + reply + "\n \n"
+
+        print(Fore.GREEN + reply + Style.RESET_ALL)
+
+        # Predict is string of an int because text classifier
+        # mapping takes str of respective numbers
+        # to identify clustered nodes from the KMeans model
+        predict = "-1"
+
+        while True:
+            if predict != "-1":
+                occurance_log = ""
+            self.message = "User's response: "
+            self.message += input("Enter Your Response: ")
+
+            if "ANALYSIS COMPLETE" in self.message:
+
+
+                # with open("Conversation.txt", "w") as text_file:
+                #     text_file.write(save_conversation)
+
+                # shutil.move("Conversation.txt",
+                #             str(Path.home()) + "/HRIPapers/Experiments/Conversation.txt"
+                #             )
+                
+                save_conversation += "User response: " + self.message[21:] + "\n \n"
+                reply = self.conversation.predict(input=self.message)
+                save_conversation += "AI response: " + reply + "\n \n"
+                # shutil.move(str(Path.home()) + "/.ros/log/CommandServer.log",
+                #             str(Path.home()) + "/HRIPapers/Experiments/CommandServer.log"
+                # )
+                print(Fore.GREEN + "Experiment Complete!" + Style.RESET_ALL)
+                return
+                
+            save_conversation += "User response: " + self.message[21:] + "\n \n"
+
+            reply = self.conversation.predict(input=self.message)
+            print(Fore.GREEN + reply + Style.RESET_ALL)
+
+            save_conversation += "AI response: " + reply + "\n \n"
+            # print('save conversation:', save_conversation)
+                
+
 if __name__ == "__main__":
-    # graph = TelemetryGraph()
-    # graph.parse_from_file("llm_telemetry/saves/telemetry/telemetry_test.txt")
+    graph = TelemetryGraph()
+    graph.parse_from_file("llm_telemetry/saves/telemetry/telemetry_test.txt")
+
+    tt = textBot()
+    tt.bot_loop(graph)
 
     # test_load_from_text()
-    test_graph_updates()
+    # test_graph_updates()
