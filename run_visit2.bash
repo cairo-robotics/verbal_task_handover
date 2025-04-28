9a446e5df8009c92088cccd271846693d88e4a9e
kalebishop <kabi2483@colorado.edu>
2025-04-28 09:30:11 -0600

    modify visit2 script to conditionally launch tutorial and open report

--- run_visit2.bash
+++ run_visit2.bash
@@ -8,8 +8,8 @@ if [ $# -lt 2 ]; then
   exit 1
 fi
 
-PARTICIPANT_ID="$1"
-LOAD_FILE="$2"
+PARTICIPANT_ID="$2"
+LOAD_FILE="$1"
 
 SAVE_DIR="/media/cairo/T7/handover_project/participant_data"
 export SAVE_DIR
@@ -30,6 +30,45 @@ if [ -f ".env" ]; then
   export $(grep -v '^#' .env | xargs)
 fi
 
+
+if [ "$PARTICIPANT_ID" != "$LOAD_FILE" ]; then
+  # Run the tutorial
+  python3 evaluation/treasure_hunt_py/treasure_hunt/scripts/tutorial.py
+  if [ $? -ne 0 ]; then
+    echo "Error: tutorial.py failed"
+    deactivate
+    exit 1
+  fi
+fi
+
+
+## ----------------------------------------------------------------------------------------------  
+# Make the report read only then open it in separate window for player 2 to reference
+## ----------------------------------------------------------------------------------------------
+
+USER_REPORT_BASENAME="${LOAD_FILE}_user_report.txt"
+echo "Report base name: $USER_REPORT_BASENAME"
+
+USER_REPORT_PATH="$SAVE_DIR/reports/$USER_REPORT_BASENAME"
+
+if [ ! -f "$USER_REPORT_PATH" ]; then
+
+  USER_REPORT_PATH="$SAVE_DIR/$USER_REPORT_BASENAME"
+fi
+
+if [ -f "$USER_REPORT_PATH" ]; then
+  echo "Loading user report from: $USER_REPORT_PATH"
+
+  chmod a-w "$USER_REPORT_PATH"
+
+  xdg-open "$USER_REPORT_PATH" &
+
+else
+  echo "Error: User report not found at expected locations"
+  exit 1
+fi
+
+## ----------------------------------------------------------------------------------------------
 
 # Run the main task (no distractor)
 python3 evaluation/treasure_hunt_py/treasure_hunt/scripts/main.py --reset-held-items --is_player2 --reset-time --load "$LOAD_FILE" --save "$PARTICIPANT_ID" >> $SAVE_DIR/$PARTICIPANT_ID.log
