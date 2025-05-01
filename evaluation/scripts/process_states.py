edfe745b26737415c56c567d07d19435b478c858
kalebishop <kabi2483@colorado.edu>
2025-05-01 10:58:30 -0600

    basic analysis of initial data collection

--- evaluation/scripts/process_states.py
+++ evaluation/scripts/process_states.py
@@ -1,20 +1,15 @@
 import os
 import sys
 from save_to_vector import output_ground_truth
-from report_to_vector import convert_to_vector
 
-PARTICIPANTS = list(range(502, 510, 2))
+# PARTICIPANTS = list(range(502, 510, 2))
+PARTICIPANTS = ["504_1", "509_1", "601", "602", "603", "604"]
 
 def main(load_dir, output_dir):
     for pid in PARTICIPANTS:
         save_file = os.path.join(load_dir, f"{pid}")
         save_output_file = os.path.join(output_dir, f"{pid}_save_output.json")
         output_ground_truth(save_file, save_output_file)
-
-        report_file = os.path.join(load_dir, f"{pid}_user_report.txt")
-        report_output_file = os.path.join(output_dir, f"{pid}_report_output.json")
-        convert_to_vector(report_file, report_output_file)
-        print(f"Processed participant {pid}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
